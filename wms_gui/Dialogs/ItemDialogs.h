6ddc75c3a3b41459ebc166bed124e67e5337c0e8
Fally00 <rayanhisgam1234@gmail.com>
2026-03-28 01:41:23 +0200

    .

--- wms_gui/Dialogs/ItemDialogs.h
+++ wms_gui/Dialogs/ItemDialogs.h
@@ -1,16 +1,19 @@
 #pragma once
 
-#include <QWidget>
-#include <QTableWidget>
 #include "controllers/WmsControllers.h"
+#include <QTableWidget>
+#include <QWidget>
+
 
 namespace ItemDialogs {
-    // Shows the Add Item dialog. Returns true if an item was added.
-    bool showAddDialog(QWidget* parent, WmsControllers& controller);
+// Shows the Add Item dialog. Returns true if an item was added.
+bool showAddDialog(QWidget *parent, WmsControllers &controller);
 
-    // Shows the Update Item dialog. Returns true if the item was updated.
-    bool showUpdateDialog(QWidget* parent, WmsControllers& controller, QTableWidget* inventoryTable);
+// Shows the Update Item dialog. Returns true if the item was updated.
+bool showUpdateDialog(QWidget *parent, WmsControllers &controller,
+                      QTableWidget *inventoryTable);
 
-    // Prompts and deletes the selected item. Returns true if deleted.
-    bool showDeleteDialog(QWidget* parent, WmsControllers& controller, QTableWidget* inventoryTable);
-}
+// Prompts and deletes the selected item. Returns true if deleted.
+bool showDeleteDialog(QWidget *parent, WmsControllers &controller,
+                      QTableWidget *inventoryTable);
+} // namespace ItemDialogs
